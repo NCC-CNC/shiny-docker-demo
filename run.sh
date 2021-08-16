46f0efefab6fe26c2a8554513b0902064706c96b
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2021-08-16 15:45:20 +1200

    tweak logging cmd

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-ls -la /logs
-
 # launch application and save log files
 /usr/local/bin/Rscript -e \
   "options(shiny.port=3838,shiny.host='0.0.0.0');shiny::runApp()" \
-  > /logs/log.txt 2>&1
+  2>&1 | tee /logs/log.txt
